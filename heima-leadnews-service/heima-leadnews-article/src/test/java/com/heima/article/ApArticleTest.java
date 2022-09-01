d5bae7ced5492ffe62f9ac445c5885039253add9
lucky_jzc <lucky@jzc.cn>
2022-09-01 19:39:09 +0800

    leadnews: Day03 1、App文章详情：功能实现 Freemarker+MinIO NO.Two

--- heima-leadnews-service/heima-leadnews-article/src/test/java/com/heima/article/ApArticleTest.java
+++ heima-leadnews-service/heima-leadnews-article/src/test/java/com/heima/article/ApArticleTest.java
@@ -17,9 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.StringWriter;
+import javax.swing.plaf.PanelUI;
+import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -49,14 +48,17 @@ public class ApArticleTest {
     @Test
     public void testUploadFile() throws Exception {
 
+        //需要生成文章的id
         Long id = 1383827787629252610L;
         QueryWrapper<ApArticleContent> queryWrapper = new QueryWrapper<>();
+        //查询条件为id
         queryWrapper.eq("article_id", id);
+
         ApArticleContent apArticleContent = apArticleContentMapper.selectOne(queryWrapper);
 
         //判断对象和内容不为空
         if (apArticleContent != null && StringUtils.isNotBlank(apArticleContent.getContent())) {
-            //读取文章详情页模板
+            //读取文章详情页模板(静态)
             Template template = configuration.getTemplate("article.ftl");
 
             Map<String, Object> map = new HashMap<>();
@@ -81,6 +83,115 @@ public class ApArticleTest {
             apArticleMapper.updateById(apArticle);
 
             //释放资源
+            inputStream.close();
+            writer.close();
+
+
+        }
+
+
+    }
+
+    /**
+     * 为文章生成详情静态页面2
+     */
+
+    @Test
+    public void testUploadFile2() throws Exception {
+        Long id = 1383827787629252610L;
+
+        //设置查询条件
+        QueryWrapper<ApArticleContent> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("article_id", id);
+        ApArticleContent apArticleContent = apArticleContentMapper.selectOne(queryWrapper);
+
+        //判断是否为空 和是否有内容
+        if (apArticleContent != null && StringUtils.isNotBlank(apArticleContent.getContent())) {
+
+            //读取文章详情模板
+            Template template = configuration.getTemplate("article.ftl");
+
+            Map<String, Object> map = new HashMap<>();
+            //将查询到的文档转为json存到map集合中
+            List<Map> context = JsonUtils.toList(apArticleContent.getContent(), Map.class);
+            map.put("context", context);
+            //字符缓冲流
+            StringWriter writer = new StringWriter();
+
+
+            //生成模板
+            template.process(map, writer);
+            System.out.println("writer = " + writer);
+            System.out.println("template = " + template);
+            System.out.println("map = " + map);
+
+
+            //上传到minion上的文件名字
+            String fileName = id + ".html";
+            InputStream inputStream = new ByteArrayInputStream(writer.toString().getBytes());
+
+            //上传到minio
+            String url = minIOFileStorageService.uploadHtmlFile(null, fileName, inputStream);
+            ApArticle apArticle = new ApArticle();
+            apArticle.setId(id);
+            apArticle.setStaticUrl(url);
+            apArticleMapper.updateById(apArticle);
+
+            //关闭资源
+            inputStream.close();
+            writer.close();
+
+
+        }
+
+    }
+
+
+    /**
+     * 为文章生成静态页面
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testUploadFile3() throws Exception {
+
+        Long id = 1383827787629252610L;
+        //设置条件
+        QueryWrapper<ApArticleContent> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("article_id", id);
+        ApArticleContent apArticleContent = apArticleContentMapper.selectOne(queryWrapper);
+
+        //判断是否为空和是否有内容
+        if (apArticleContent != null && StringUtils.isNotBlank(apArticleContent.getContent())) {
+            //读取文章模板
+            Template template = configuration.getTemplate("article.ftl");
+
+
+            //把内容转为json 存储到Map集合中
+            List<Map> context = JsonUtils.toList(apArticleContent.getContent(), Map.class);
+
+            Map<String, Object> map = new HashMap<>();
+            map.put("context", context);
+
+            //生成文章的静态页面
+            StringWriter writer = new StringWriter();
+            template.process(map, writer);
+            InputStream inputStream = new ByteArrayInputStream(writer.toString().getBytes());
+
+            String fileName = id + ".html";
+
+            //上传文件
+            String url = minIOFileStorageService.uploadHtmlFile(null, fileName, inputStream);
+
+            //根据id设置url
+            ApArticle apArticle = new ApArticle();
+            apArticle.setStaticUrl(url);
+            apArticle.setId(id);
+
+            //修改内容
+            apArticleMapper.updateById(apArticle);
+
+            //关闭资源
             inputStream.close();
             writer.close();
 
